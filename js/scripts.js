93a23781905c2041953823fa37e4e9f8dc392600
Ohene Gyan <Janmillz@yahoo.com>
2017-09-23 17:04:35 +0100

    modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
-    }
+  $("form#number").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var result = number(number);
+    $("#result").text(result);
+  });
+});
